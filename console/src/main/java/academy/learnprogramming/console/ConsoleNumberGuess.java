92dc62dc14d9ae1dd2ca6dcd8c08073d3e323b7b
Roberto <boby_a94@hotmail.com>
2021-02-07 17:29:26 +0100

    Used lombok to remove boilerplate code

--- console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
+++ console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
@@ -2,8 +2,7 @@ package academy.learnprogramming.console;
 
 import academy.learnprogramming.Game;
 import academy.learnprogramming.MessageGenerator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -12,9 +11,8 @@ import org.springframework.stereotype.Component;
 import java.util.Scanner;
 
 @Component
+@Slf4j
 public class ConsoleNumberGuess {
-    // == constants ==
-    private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
     // == constructor ==
 
--- core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -1,5 +1,6 @@
 package academy.learnprogramming;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +13,10 @@ public class NumberGeneratorImpl implements NumberGenerator{
     // == fields ==
     private final Random random = new Random();
 
+    @Getter
     private final int maxNumber;
 
+    @Getter
     private final int minNumber;
 
     // == constructor ==
@@ -28,15 +31,5 @@ public class NumberGeneratorImpl implements NumberGenerator{
     @Override
     public int next() {
         return random.nextInt((maxNumber-minNumber) + minNumber );
-    }
-
-    @Override
-    public int getMaxNumber() {
-        return maxNumber;
-    }
-
-    @Override
-    public int getMinNumber() {
-        return minNumber;
     }
 }
